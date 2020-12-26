ada2804dd9d7c9db504c1f08230d497fc47390a6
cengizcmataraci <cengizcmataraci@gmail.com>
2020-12-26 19:35:46 +0300

    Add contact

---
+++ src/components/Contact.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="container text-center mt-4">
+  <div class="row">
+    <div class="col-12">
+      <h1>İletişim</h1>
+    </div>
+    <div class="col-12">
+<form>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputEmail4">Mail Adresiniz:</label>
+      <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+    </div>
+  </div>
+  <div class="form-group">
+  <button type="submit" class="btn btn-primary">Gönder</button>
+  </div>
+</form>
+    </div>
+    <div class="col-12">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6019.774373934914!2d28.972274000000002!3d41.027724!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x1609c6e2b7154d9f!2zxLBCQiBaZW1pbiDEsHN0YW5idWw!5e0!3m2!1sen!2sus!4v1609000454623!5m2!1sen!2sus" width="400" height="250" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+    </div>
+  </div>
+</div>
+</template>
