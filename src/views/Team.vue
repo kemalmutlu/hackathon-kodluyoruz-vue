05f2885a3f1ff4b3dbec74be0b0811d4bd400050
ferhatdemir <edemirferhat@gmail.com>
2020-12-26 19:47:18 +0300

    Fix logo issue

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -52,7 +52,7 @@ export default {
 </script>
 
 <template>
-  <div class="hackathon-detail-wrapper">
+  <div class="hackathon-team-wrapper">
     <Navbar />
     <div class="hackathon-detail">
       <content-loader v-if="isLoading" />
@@ -97,7 +97,7 @@ $content-color: rgb(75, 85, 99);
 $box-shadow: 0 0.5em 1em -0.125em rgba(10, 10, 10, 0.1),
   0 0 0 1px rgba(10, 10, 10, 0.02);
 
-.hackathon-detail-wrapper {
+.hackathon-team-wrapper {
   max-width: 1280px;
   width: 100%;
   margin: 0 auto;
@@ -109,30 +109,9 @@ $box-shadow: 0 0.5em 1em -0.125em rgba(10, 10, 10, 0.1),
     text-align: center;
     padding: 1rem 4rem;
     box-shadow: $box-shadow;
-
-    &__header {
-      &-logo {
-        width: 120px;
-        height: 120px;
-      }
-
-      &-title {
-        margin: 0;
-      }
-
-      &-date {
-        margin: 1rem 2rem 0 0;
-      }
-    }
-
-    &__content {
-      white-space: pre-line;
-      color: $content-color;
-      line-height: 1.75rem;
-      padding-bottom: 2rem;
-    }
   }
 }
+
 .location-contain {
   display: flex;
   justify-content: center;
@@ -148,6 +127,7 @@ $box-shadow: 0 0.5em 1em -0.125em rgba(10, 10, 10, 0.1),
   border: 1px solid #ddd;
   padding: 20px 20px;
   margin: 10px;
+
   h2 {
     margin: 0;
     text-align: center;
