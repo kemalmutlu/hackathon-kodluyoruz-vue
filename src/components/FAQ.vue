de71a12aadd2504081419539f5511cd98d6edc42
Kemal MUTLU <mutlukemal1717@gmail.com>
2020-12-26 17:29:00 +0300

    Add faq component

--- src/components/FAQ.vue
+++ src/components/FAQ.vue
@@ -1,42 +1,45 @@
 <script>
+import Vue from 'vue';
+import { AccordionMenu } from 'vue-accordion-menu';
+
+Vue.component('AccordionMenu', AccordionMenu);
+
 export default {
   data() {
     return {
-      firstQuestion: false,
-      secondQuestion: false,
+      contents: [
+        {
+          title: 'Backend gibi herhangi bir teknoloji kullanabilir miyim?',
+          msg: 'Evet. Her bir takım için backend veya herhangi bir teknoloji kullanmaları tamamen serbest olup puanlamada ek olarak avantaj sağlayacağından emin olabilirsiniz. Fakat backend veya web frameworklerinin olmaması takımınızı bu etkinlikte elemez.',
+        },
+        {
+          title: 'Siteye yukarıda belirtilmeyen fakat eklenmesi gerektiğini düşündüğüm bazı detayları ekleyebilir miyim?',
+          msg: 'Tabii ki. Kodluyoruz Hackathon tamamen yeni fikirlere ve ayrıcalıklara özen gösteren ve herkesin aklında olanları kodlara dökmek için tamamen serbest olduğu bir etkinliktir.',
+        },
+      ]
     };
   },
 }
 </script>
 
 <template>
-  <section class="accordions">
-    <article class="accordion" @click="firstQuestion = !firstQuestion">
-      <p>Backend gibi herhangi bir teknoloji kullanabilir miyim?</p>
-        <div v-if="firstQuestion" class="accordion-body">
-          Evet. Her bir takım için backend veya herhangi bir teknoloji kullanmaları tamamen serbest olup puanlamada ek olarak avantaj sağlayacağından emin olabilirsiniz. Fakat backend veya web frameworklerinin olmaması takımınızı bu etkinlikte elemez.
-        </div>
-    </article>
-    <article class="accordion" @click="secondQuestion = !secondQuestion">
-      <p>Siteye yukarıda belirtilmeyen fakat eklenmesi gerektiğini düşündüğüm bazı detayları ekleyebilir miyim?</p>
-      <div v-if="secondQuestion" class="accordion-body">
-        Tabii ki. Kodluyoruz Hackathon tamamen yeni fikirlere ve ayrıcalıklara özen gösteren ve herkesin aklında olanları kodlara dökmek için tamamen serbest olduğu bir etkinliktir.
-      </div>
-    </article>
-  </section>
+  <div class="faq_list">
+    <h1>Sık sorulan sorular</h1>
+    <AccordionMenu :contents="contents"></AccordionMenu>
+  </div>
 </template>
 
 <style lang="scss">
-.accordions {
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  margin-top: 2rem;
-
-  .accordion {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+.faq_list{
+  h1{
+    text-align: center;
+    margin-top: 2em;
   }
+  .faq-description{
+    .faq-number{
+      display: none;
+    }
+  }
+
 }
 </style>
