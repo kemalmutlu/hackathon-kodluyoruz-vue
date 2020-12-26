59ff8acabd3b94cbc0bdd2d8da0e20adb446c6e9
ferhatdemir <edemirferhat@gmail.com>
2020-12-26 19:13:10 +0300

    Update design

--- src/components/Sponsor.vue
+++ src/components/Sponsor.vue
@@ -1,24 +1,35 @@
 <template>
-  <div class="container">
+  <div class="sponsor">
+    <h2 class="h1">Sponsorlar</h2>
     <div class="row">
-      <div class="col-12 text-center">
-        <h2 class="h1">Sponsorlar</h2>
-      </div>
-      <div class="col-4">
-        <div class="card border-0">
-          <img src="../assets/Sponsors/kablosuzbeyin_500x.png" class="card-img-top" alt="Kablosuz Beyin">
-        </div>
-      </div>
-      <div class="col-4">
-        <div class="card border-0">
-          <img src="../assets/Sponsors/kodluyoruz.png" class="card-img-top" alt="Kodluyoruz">
-        </div>
-      </div>
-      <div class="col-4">
-        <div class="card border-0">
-          <img src="../assets/Sponsors/robotsan.png" class="card-img-top" alt="Robotsan">
-        </div>
-      </div>
+      <img src="../assets/Sponsors/kablosuzbeyin_500x.png" class="card-img-top kablosuz-beyin" alt="Kablosuz Beyin">
+      <img src="../assets/kodluyoruz-logo.png" class="card-img-top" alt="Kodluyoruz">
+      <img src="../assets/Sponsors/robotsan.png" class="card-img-top" alt="Robotsan">
     </div>
   </div>
 </template>
+
+<style lang="scss">
+.sponsor {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 24px;
+
+  .row {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    img {
+      width: 18%;
+      margin: 0 24px;
+
+      &:first-child {
+        width: 10%;
+      }
+    }
+  }
+}
+</style>
